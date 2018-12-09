658335291d2533eaedccb85b2419fdb728cc45b8
omerh <omerha@protonmail.com>
2018-12-10 01:21:17 +0200

    cleanup

--- main.go
+++ main.go
@@ -13,7 +13,6 @@ import (
 type fileInfo struct {
 	name string
 	size int64
-	// checksum []byte
 }
 
 type filesInfo struct {
@@ -58,19 +57,14 @@ func main() {
 			f, err := os.Open(filepath.Join(filePath, file.Name()))
 			check(err)
 			defer f.Close()
-			// h := md5.New()
-			// if _, err := io.Copy(h, f); err != nil {
-			// 	check(err)
-			// }
 			fi := new(fileInfo)
 			fi.name = file.Name()
 			fi.size = file.Size()
-			// fi.checksum = h.Sum(nil)
+
 			// log.Printf("Adding to slice %v", fileInfo{file.Name(), file.Size(), h.Sum(nil)})
 			filesInformation = append(filesInformation, fi)
 		}
 	}
-
 	// Creating files list for comparison inorder to delete original file information list
 	compareFilesSlice := filesInformation
 
@@ -91,19 +85,5 @@ func main() {
 		}
 		// Removing from original slice the item that was compared
 		filesInformation = append(filesInformation[:i], filesInformation[i+1:]...)
-
-		// Not removing elements in iteration causing a double enrty of duplicates
-		// for _, s := range filesInformation {
-		// 	for _, d := range filesInformation {
-		// 		if s.name != d.name {
-		// 			// log.Printf("Comparing file %v to %v", s.name, d.name)
-		// 			if s.size == d.size {
-		// 				// log.Printf("Comprating checksum of %v with %x to %v with %x", s.name, s.checksum, d.name, d.checksum)
-		// 				if bytes.Equal(s.checksum, d.checksum) {
-		// 					log.Printf("Files %v and %v are identical with size %v and hash of %x", s.name, d.name, s.size, s.checksum)
-		// 				}
-		// 			}
-		// 		}
-		// 	}
 	}
 }
